7fcdd680ad18279f87f1a9a6712adaaf552033c7
DattiVishnuVandana <vandanadatti567@gmail.com>
2025-03-01 08:19:37 +0530

    usestates-basic counter

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+const handleAdd=()=>{
+  setCount(count+1)
+}
+const handleSub=()=>{
+  setCount(count-1)
+}
+  return (
+    <>
+    <div className="flex justify-center my-10">
+  <div className="card bg-slate-500 text-neutral-content w-96 ">
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">COUNTER!</h2>
+    <p>{count}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary" onClick={handleAdd} >ADD</button>
+      <button className="btn btn-ghost" onClick={handleSub}>SUB</button>
+    </div>
+  </div>
+</div></div>
+    </>
+  )
+}
+
+export default App
