0df3e3412a1d9ae7a59cec9602ecbef092ee544e
DattiVishnuVandana <vandanadatti567@gmail.com>
2025-03-08 10:49:50 +0530

    basic hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 
 import './App.css'
-import TaskList from './components/TaskList'
-import Header from './components/Header'
-import AddTask from './components/AddTask'
-import { useState } from 'react'
+
 
 function App() {
 
-  const [tasks,setTasks]=useState([{id:1234,name:"React-js",completed:true},
-        {id:1235,name:"node-js",completed:false},
-        {id:1236,name:"Gate",completed:true}
-      ])
- const stuff="random"
+
   return (
     <>
-    
-    <Header/>
-    <main>
-    <AddTask tasks={tasks} setTasks={setTasks} />
-     <TaskList  tasks={tasks} setTasks={setTasks} info={stuff}/>
-     </main>
+ 
+
      
       </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import About from "./components/About"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <Header/>
+  <About/>
+ 
+       <App />
+ <Footer/>
   </StrictMode>,
 )
