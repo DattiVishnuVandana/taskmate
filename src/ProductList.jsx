8ce70730a34b37bd77849202ceeb92c5850b1c6b
DattiVishnuVandana <vandanadatti567@gmail.com>
2025-03-08 12:10:36 +0530

    list-o/p,onclick events

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from 'react'
 
 const productList = () => {
+    const [cnt,setCnt]=useState(0)
     const [products,setProducts]=useState([]);
+    const [url,setUrl]=useState("http://localhost:8001/products")
+    // console.log(products);
+    // console.log("outside useeffect-render");
+    useEffect(()=>{
+        fetch(url).then(res=>res.json()).then(data=>setProducts(data))
+// console.log("useeffect render");
+
+    },[url])
     // console.log(products);
     useEffect(()=>{
-        fetch("http://localhost:8001/products").then(res=>res.json()).then(data=>setProducts(data))
-
-    },[])
-    console.log(products);
+        console.log(cnt);
+    },[cnt])
   return (
     <div>
-   
-
+        <button onClick={()=>setCnt(cnt+1)}>{cnt}</button>
+   <button onClick={()=>setUrl("http://localhost:8001/products")}>all</button>
+   <button onClick={()=>setUrl("http://localhost:8001/products?in_stock=true")}>InStock</button>
+{products.map((product)=>(
+    <p key={product.id}>{product.name}-price:${product.price} -{product.in_stock?"InStcock":"outStcock"}</p>
+))}
     </div>
   )
 }
